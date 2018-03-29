3769a4d25bfa5f951679693dddad3b43d10ae007
Jacob Swanson <jacob.swanson.n@gmail.com>
2018-03-29 08:15:04 -0500

    Get basic zoom to work

--- poe-simulator/src/components/canvas/Stage.tsx
+++ poe-simulator/src/components/canvas/Stage.tsx
@@ -1,6 +1,9 @@
 import React = require('react');
 import { LoggerFactory } from '../../utils/logger/LoggerFactory';
 import { Stage as PixiStage } from 'react-pixi-fiber';
+import * as ReactDOM from 'react-dom';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
 const log = LoggerFactory.forClass('Stage');
 
@@ -11,9 +14,42 @@ export interface StageProps {
     draggable?: boolean;
 }
 
+@observer
 export class Stage extends React.Component<StageProps> {
-    // @observable scale: number = 1;
+    @observable scale: number = 1;
     // @observable position: Vector2d = {x: 0, y: 0};
+
+    private stage: PixiStage | null = null;
+
+    componentDidMount() {
+        if (!this.stage) {
+            return;
+        }
+        const node = ReactDOM.findDOMNode(this.stage);
+        node.addEventListener('mousewheel', this.onContentWheel);
+    }
+
+    onContentWheel = (e: any) => {
+        const delta = e.deltaY || e.wheelDelta;
+        this.scale = delta > 0 ? this.scale * 1.5 : this.scale / 1.5;
+        // const oldScale = this.scale;
+        // const pointerPosition = this.stage.getStage().getPointerPosition();
+        // const stagePosition = this.stage.getStage().getPosition();
+        //
+        // const mousePointsTo = {
+        //     x: pointerPosition.x / oldScale - stagePosition.x / oldScale,
+        //     y: pointerPosition.y / oldScale - stagePosition.y / oldScale
+        // };
+        //
+        // const scaleBy = 1.5;
+        // const newScale = delta < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+        //
+        // this.position = {
+        //     x: -(mousePointsTo.x - pointerPosition.x / newScale) * newScale,
+        //     y: -(mousePointsTo.y - pointerPosition.y / newScale) * newScale
+        // };
+        // this.scale = newScale;
+    };
 
     // onContentWheel = (e: any) => {
     //     if (!this.stage) {
@@ -41,17 +77,20 @@ export class Stage extends React.Component<StageProps> {
     // };
 
     render() {
-        const scale = new PIXI.Point(0.2, 0.2);
+        const scale = new PIXI.Point(this.scale, this.scale);
         return (
-            <PixiStage
-                width={window.innerWidth}
-                height={window.innerHeight}
-                scale={scale}
-                x={1000}
-                y={600}
-            >
-                {this.props.children}
-            </PixiStage>
+            <div className="Stage">
+                <PixiStage
+                    ref={(stage) => this.stage = stage}
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                    scale={scale}
+                    x={1000}
+                    y={600}
+                >
+                    {this.props.children}
+                </PixiStage>
+            </div>
         );
     }
 }
