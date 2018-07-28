4b4125ca4c9cc5480e208490c7fbb5ab4e8062df
Jacob Swanson <jacob.swanson.n@gmail.com>
2018-07-28 17:49:55 -0500

    Get my own canvas renderer working

---
+++ react-pixi-fiber/src/ReactPixiRenderer.js
@@ -0,0 +1,61 @@
+// @flow
+
+import Reconciler from "react-reconciler";
+
+export const ReactPixiRenderer = Reconciler({
+    appendInitialChild(parentInstance, child) {
+        if (parentInstance.appendChild) {
+            parentInstance.appendChild(child);
+        } else {
+            parentInstance.document = child;
+        }
+    },
+
+    createInstance(type, props, internalInstanceHandle) {
+        return createElement(type, props, internalInstanceHandle);
+    },
+
+    createTextInstance(text, rootContainerInstance, internalInstanceHandle) {
+        return text;
+    },
+
+    finalizeInitialChildren(wordElement, type, props) {
+        return false;
+    },
+
+    getPublicInstance(inst) {
+        return inst;
+    },
+
+    prepareForCommit() {
+        // noop
+    },
+
+    prepareUpdate(wordElement, type, oldProps, newProps) {
+        return true;
+    },
+
+    resetAfterCommit() {
+        // noop
+    },
+
+    resetTextContent(wordElement) {
+        // noop
+    },
+
+    getRootHostContext(rootInstance) {
+        // You can use this 'rootInstance' to pass data from the roots.
+    },
+
+    getChildHostContext() {
+        return emptyObject;
+    },
+
+    shouldSetTextContent(type, props) {
+        return false;
+    },
+
+    now: () => {},
+
+    supportsMutation: false
+});
